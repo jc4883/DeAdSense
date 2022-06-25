35459053f3b7bcc5cdc2ada5730b71b47605817f
JunHyeok Kwak <starjun9123@gmail.com>
2022-06-25 13:42:29 -0400

    some fe stuff

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,8 +1,27 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+
+let theme = createTheme({
+  palette: {
+    primary: {
+      light: "#C8C2FF",
+      main: "#857AFD",
+      dark: "#38308A",
+      contrastText: "#000",
+    },
+  },
+  typography: {
+    fontFamily: ["Jost"].join(","),
+  },
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
